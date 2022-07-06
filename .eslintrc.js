af71db8a58527ff3f4f0b3329f42813e11125b91
PadillaH <ramon.h.padilla@gmail.com>
2022-07-06 16:24:36 -0500

    Config. inicial

---
+++ .eslintrc.js
@@ -0,0 +1,33 @@
+const RULES = {
+  OFF: 'off',
+  WARN: 'warn',
+  ERROR: 'error'
+}
+
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+    jest: true
+  },
+  extends: [
+    'plugin:react/recommended',
+    'standard'
+  ],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true
+    },
+    ecmaVersion: 'latest',
+    sourceType: 'module'
+  },
+  plugins: [
+    'react'
+  ],
+  rules: {
+    'react/react-in-jsx-scope': RULES.OFF,
+    'react/prop-types': RULES.OFF,
+    'react/display-name': RULES.OFF
+  }
+}
